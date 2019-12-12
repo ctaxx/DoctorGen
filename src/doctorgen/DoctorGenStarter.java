310f0c459b6a69766fbc8c1a3ddf0af3df83ce53
ctaxx <bykov.stanislav.p@gmail.com>
2019-12-12 21:50:02 +0200

    working with table in initial panel

--- src/doctorgen/DoctorGenStarter.java
+++ src/doctorgen/DoctorGenStarter.java
@@ -23,7 +23,7 @@ public class DoctorGenStarter extends Frame {
 	public DoctorGenStarter() {
 		super("RTF generator");
 
-		setSize(300, 200);
+		setSize(500, 200);
 		setLayout(new BorderLayout());
 
 		centralPanel = new Panel();
--- src/doctorgen/flowpanels/InitialPanel.java
+++ src/doctorgen/flowpanels/InitialPanel.java
@@ -8,11 +8,16 @@ import java.awt.TextField;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 public class InitialPanel extends FlowPanel{
 	TextField nameTextField, projectTextField, quantityTextField;
-	JTable serials;
+	JTable serialsTable;
+	Object [][] serialsArray;
+	int quantity = 1;
+	
+	Choice quantityChoice;
 	
 	public InitialPanel(){
 		super();
@@ -30,7 +35,7 @@ public class InitialPanel extends FlowPanel{
 		quantityPanel.setLayout(new FlowLayout());
 		Label quantityLabel = new Label("quantity");
 		quantityPanel.add(quantityLabel);
-		Choice quantityChoice = new Choice();
+		quantityChoice = new Choice();
 		quantityChoice.add("1");
 		quantityChoice.add("2");
 		quantityChoice.add("3");
@@ -39,11 +44,36 @@ public class InitialPanel extends FlowPanel{
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				System.out.println("choice was changed");
-				
+				quantity = Integer.parseInt(quantityChoice.getSelectedItem());
+				serialsArray = createSerialsArray(quantity);
 			}
 		});
 		quantityPanel.add(quantityChoice);
 		add(quantityPanel);
 		
+		Panel tablePanel = new Panel();
+		
+		
+		String [] colNames = {"number", "serial"};
+//		Object [][] data = {{"A bugs life", "G"},
+//				{"Civil Action", "PG13"}};
+//		JTable table = new JTable(data, colNames);
+		quantity = Integer.parseInt(quantityChoice.getSelectedItem());
+		serialsArray = createSerialsArray(quantity);
+		serialsTable = new JTable(serialsArray, colNames);
+		
+		tablePanel.add(new JScrollPane(serialsTable));
+		add(tablePanel);
+		
+	}
+	
+	private Object [][] createSerialsArray(int quantity){
+		Object [][] objArray = new Object[quantity][2];
+		
+		for (int i = 0; i < quantity; i++){
+			objArray[i][0]= Integer.toString(i + 1);
+			objArray[i][1]= "";
+		}
+		return objArray;
 	}
 }
